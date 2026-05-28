03d3e8f7f85192d42d94981a57f9259b89bba00e
florent <florent.donat.bouillud@gmail.com>
2026-05-28 17:22:30 +0200

    added a test file to test hypothesis and functions

--- model.py
+++ model.py
@@ -1,10 +1,29 @@
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 
 class Stacked_Conv(nn.Module):
-    def __init__(self):
+    def __init__(self, nb_stack = 3, nb_layers = 10):
         super(Stacked_Conv, self).__init__()
-        for i in range
+        
+        dilations = [2**i for i in range(nb_layers)] * nb_stack
+
+        self.convs = nn.ModuleList([
+            nn.Conv1d(
+                in_channels=256,
+                out_channels=256,
+                kernel_size=2,
+                dilation=d,
+                padding=0
+            )
+            for d in dilations
+        ])
+
+    def forward(self, x):
+        for conv in self.convs:
+            pad_left = (conv.kernel_size[0] - 1) * conv.dilation[0]
+            x = F.pad(x, (pad_left,0))
+            x= conv(x)
+        return x
 
 
---
+++ test.py
@@ -0,0 +1,41 @@
+import os
+import torch
+import torch.nn as nn
+import soundfile as sf
+import torchaudio.functional as F
+from model import Stacked_Conv
+
+AUDIO_PATH = "/home/florent/Dataset/CSTR_VCTK/wav48_silence_trimmed/p228/"
+audio, sr = sf.read(os.path.join(AUDIO_PATH, "p228_022_mic2.flac"), dtype="float32")
+audio = torch.from_numpy(audio)
+audio_16k = F.resample(audio, orig_freq=sr, new_freq=16000)
+audio = F.mu_law_encoding(audio_16k, 256)
+audio = nn.functional.one_hot(audio, num_classes=256).float()
+audio = audio.T.unsqueeze(0)
+
+print(audio.shape)
+
+def test_causality():
+    model = Stacked_Conv()
+
+    model.eval()
+
+    x1 = audio.clone()
+    x2 = audio.clone()
+
+    t = 1000
+
+    future_len = x2.shape[-1] - (t + 1)
+    random_classes = torch.randint(0, 256, (1, future_len))
+    random_future = torch.nn.functional.one_hot(random_classes, num_classes=256).float()
+    random_future = random_future.permute(0, 2, 1)  # [1, 256, future_len]
+    print(random_future)
+    x2[:, :, t+1:] = random_future
+
+    y1 = model(x1)
+    y2 = model(x2)
+
+    diff = (y1[:, :, :t+1] - y2[:, :, :t+1]).abs().max().item()
+    print(diff)
+
+test_causality()
